6bb5dbf516499c10ad31b4d0d69a2ca195961c5a
Bgmilodhi <bgmilodhi0@gmail.com>
2024-10-20 17:54:20 +0530

    Add files via upload

--- BROTHERHOOD.py
+++ BROTHERHOOD.py
@@ -4,8 +4,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 from telegram.error import TelegramError
 
-TELEGRAM_BOT_TOKEN = '7880546265:AAj5c5PTqfJ7Lx22o7pj5MPa4ckTjuU'
-ALLOWED_USER_ID = 7372592  
+TELEGRAM_BOT_TOKEN = '7797034995:AAHob8SQ8K3JWaPYFjM-Gkru3c6aJtjpzm8'
+ALLOWED_USER_ID = 1903241093  
 bot_access_free = True  
 
 async def start(update: Update, context: CallbackContext):
